f5603c14e66bb5c4735d81a27d68f5b4866ab80f
fatal-zio <40217128+fatal-zio@users.noreply.github.com>
2018-09-28 09:34:46 -0500

    Updated gitignore and removed redundant logging statement.

--- Startup.cs
+++ Startup.cs
@@ -35,8 +35,6 @@ namespace CityInfoApi
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory, CityInfoContext cityInfoContext)
         {
-            loggerFactory.AddConsole();       
-
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
